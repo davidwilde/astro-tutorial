c524718b4a09dc0baf1b31943d13d544d3d9919f
David Wilde <d.wilde@selligence.com>
2023-09-06 14:54:03 +0100

    Astro tutorial with htmx and tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+    output: "hybrid",
+    integrations: [preact(), tailwind()]
+});
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET() {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'http://127.0.0.1:4321my-blog-site.netlify.app',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
